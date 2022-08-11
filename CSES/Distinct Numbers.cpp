7922456e43c15d95041416c5c7a91d3c11684c24
imeabdullah <abdullahprime98@gmail.com>
2022-08-11 09:14:56 +0600

    Distinct Numbers

---
+++ CSES/Distinct Numbers.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0;i<n;i++) {
+        cin>> arr[i];
+    }
+    sort(arr,arr+n);
+    int count=0;
+    for(int i = 0;i<n;){
+        int j = i;
+        while(arr[i] == arr[j]) {
+            j++;
+        }
+        count++;
+        i=j;
+    }
+    cout<<count<<endl;
+}
